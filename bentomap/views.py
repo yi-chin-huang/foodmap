75b1ae39a2de215fadb0048b7d86e8fcc1535499
Li Yi-Jie <liyi-jie@Lide-MacBook-Pro.local>
2018-11-23 03:56:37 +0800

    debug

---
+++ bentomap/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate
+from django.contrib import auth
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+
+def home(request):
+	return render(request, 'home.html')
