3edb16ee4747c12867e4adf510838c199a57ff4f
Li Yi-Jie <liyi-jie@Lide-MacBook-Pro.local>
2018-11-27 03:54:45 +0800

    start account app, add it in setting, and linked in homepage

--- bentomap/urls.py
+++ bentomap/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/',include('accounts.urls')),
     path('', views.home),
-    # path('accounts/',include('accounts.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
