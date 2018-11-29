e248e9fda6f60876da7581e12bfb0547c78c1c09
黃意芹 <ctps930080@gmail.com>
2018-11-30 03:14:07 +0800

    del map widgets

--- bentomap/settings.py
+++ bentomap/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'foodevent',
     'accounts',
-    'django.contrib.gis',
+    # 'django.contrib.gis',
     # 'easy_maps',
     # 'mapwidgets',
 ]
